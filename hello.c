f6af7bc099ac0421faae8c52f07c887eba52a7df
tuyoik <andoonote@gmail.com>
2024-06-28 04:49:46 +1000

    initial commit

---
+++ hello.c
@@ -0,0 +1,18 @@
+/**
+ * ANDOO NOTE
+ * 2024-06-27
+ *
+ * A hello world style program that prints andoo's name
+ */
+
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(int argc, char** argv){
+
+
+    printf("Hello Pandoo!\n");
+
+    return 0;
+
+}
